43dfc0e186856dcf00313970d5887ddb7a307ef5
Vinicius Souza <91327153+Vinicius-Souza-Araujo@users.noreply.github.com>
2023-04-07 20:57:57 -0300

    Update Program.cs

--- lista-exercicios-01/Ex13/Ex13/Program.cs
+++ lista-exercicios-01/Ex13/Ex13/Program.cs
@@ -4,6 +4,10 @@
     {
         static void Main(string[] args)
         {
+            //13) Uma loja vende bicicletas com um acréscimo de 50% sobre o preço de custo. Ela paga a cada vendedor 2 salários mínimos mensais, mais uma comissão de 15% sobre o 
+            //preço de custo de cada bicicleta vendida. Desenvolva um algoritmo que leia o valor do salário mínimo, o preço de custo de cada bicicleta e o número de bicicletas 
+            //vendidas pelo vendedor, calcule e mostre: o salário do empregado
+            
             double salarioMinimo, precoCusto, precoVenda, numBicicletasVendidas, salarioEmpregado;
 
             Console.WriteLine("Digite o valor do salário minimo: ");
